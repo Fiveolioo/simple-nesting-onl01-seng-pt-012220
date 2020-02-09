b02486a78aa9274e9a65ff1ecf1e10ba35041e91
Richard Cho <96richardohc@gmail.com>
2020-02-09 05:09:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,6 @@ def hopper
           :languages => ["C"]
         }
      }
-  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +35,6 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +53,6 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,9 +77,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] = {}
-    
-  programmer_hash
 end
 
 def changing_alan
@@ -102,9 +96,6 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash
      
 end
 
@@ -124,6 +115,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages][1]= "Assembly"
-     programmer_hash
+     
 end
