48d9b6ebffa3a7f4dda75f5b12e4951213035028
Richard Cho <96richardohc@gmail.com>
2020-02-09 05:24:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[":dennis_ritche"]
+     programmer_hash[:dennis_ritche]("")
 end
 
 def adding_matz
